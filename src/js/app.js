de1cec320d549445e3abe1ccee9cb40c4672ddec
Cristhian <cristhianfernandolp@gmail.com>
2024-03-29 15:41:49 -0300

    feat: Minified version of the page scripts

--- src/js/app.js
+++ src/js/app.js
@@ -20,4 +20,20 @@ document.addEventListener("DOMContentLoaded", (e) => {
       }
     });
   });
+
+  // PRELOADING IMAGES
+  const blurredImageDiv = document.querySelectorAll(".blurred-img");
+  blurredImageDiv.forEach((div) => {
+    const img = div.querySelector("img");
+    function loaded() {
+      div.classList.add("loaded");
+      div.style.backgroundImage = "";
+    }
+
+    if (img.complete) {
+      loaded();
+    } else {
+      img.addEventListener("load", loaded);
+    }
+  });
 });
